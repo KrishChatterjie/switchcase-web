648a4d775b233bcddde33b72f062722148877225
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-06-09 16:13:36 +0530

    feat: added header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Header />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  --dark-blue: #2f4a67;
+  --teal-200: #b2eee6;
+  --teal-500: #8ad6cc;
+  --pink-200: #f99192;
+  --pink-500: #f97171;
+}
+
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  font-family: 'Manrope', sans-serif;
+  background-color: var(--dark-blue);
+  color: #fff;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,102 @@
+<template>
+  <header>
+    <a href="localhost:8080">
+      <div class="logo">
+        <img src="./../assets/logo.png" alt="SwitchCase Logo" />
+      </div>
+      <h1 class="switchcase">Switch<span>Case</span></h1>
+    </a>
+
+    <div class="faces">
+      <a href="https://github.com/KrishChatterjie">
+        <div class="face">
+          <img src="./../assets/kc.jpg" alt="Krish" />
+        </div>
+      </a>
+      <a href="https://github.com/iamyajat">
+        <div class="face">
+          <img src="./../assets/ym.jpeg" alt="Yajat" />
+        </div>
+      </a>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header'
+})
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 0 0.625rem rgba(0, 0, 0, 0.2);
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  width: 1.75rem;
+  height: 1.75rem;
+  margin-right: 0.25rem;
+}
+
+.logo > img {
+  width: 100%;
+}
+
+.switchcase {
+  font-size: 1.5rem;
+}
+
+.switchcase > span {
+  color: var(--pink-200);
+}
+
+.faces {
+  display: flex;
+  align-items: center;
+}
+
+.face {
+  margin-left: 0.5rem;
+  height: 2rem;
+  width: 2rem;
+}
+
+.face > img {
+  width: 100%;
+  border: 0.0625rem solid;
+  border-radius: 50%;
+}
+
+@media screen and (min-width: 768px) {
+  .logo {
+    width: 2rem;
+    height: 2rem;
+    margin-right: 0.5rem;
+  }
+
+  .switchcase {
+    font: 2rem;
+  }
+
+  .face {
+    margin-left: 0.5rem;
+    height: 2.5rem;
+    width: 2.5rem;
+  }
+}
+</style>
